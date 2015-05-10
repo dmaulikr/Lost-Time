c647c82424dac8f92abd4536dd6eb408d45f47d0
Stefan Kendall <stefankendall@gmail.com>
2015-05-09 21:04:24 -0400

    Added Track Cell. Need to save data."

--- LostTime/TrackController.m
+++ LostTime/TrackController.m
@@ -1,12 +1,22 @@
 #import "TrackController.h"
+#import "TrackCell.h"
 
 @interface TrackController ()
 @end
 
 @implementation TrackController
 
+- (void)registerCellNib:(Class)klass {
+    UINib *nib = [UINib nibWithNibName:NSStringFromClass(klass) bundle:nil];
+    [[self tableView] registerNib:nib forCellReuseIdentifier:NSStringFromClass(klass)];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self registerCellNib:TrackCell.class];
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 44;
+    self.tableView.contentInset = UIEdgeInsetsMake(20.0f, 0.0f, 0.0f, 0.0f);
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -14,8 +24,8 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+    TrackCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(TrackCell.class) forIndexPath:indexPath];
+    return cell;
 }
-
 
 @end
